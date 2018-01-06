6ba91f1946ff78dce04694d862e893a258fa8a2d
Elie NEHME <elie29@gmail.com>
2018-01-06 14:41:51 +0100

    Prepare routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,12 @@
-import {Routes} from "@angular/router";
+import { Routes } from '@angular/router';
+
+import { BookComponent } from './book/book.component';
+import { LibraryComponent } from './library/library.component';
+import { SearchComponent } from './search/search.component';
 
 export const routes: Routes = [
-  //TODO: Define the routes
+  { path: '', redirectTo: 'search', pathMatch: 'full' },
+  { path: 'search', component: SearchComponent },
+  { path: 'library', component: LibraryComponent },
+  { path: 'book/:id', component: BookComponent }
 ];
